97a268b4a28c29b32caaab27b33c61f7f91d3211
FRONT-tecnologia <samuelsalgadolife@gmail.com>
2022-02-28 08:55:26 -0300

    Criando um website completo Tema Joias

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.card').owlCarousel({
+            margin:20,
+            loop:true,
+            autoplayTimeOut:2000,
+            autoplayHoverPauser:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false
+                },
+                500:{
+                    items:2,
+                    nav:false
+                },
+                1440:{
+                    items:3,
+                    nav:false
+                }
+            }
+        })
+
+$('.btn').click(function(){
+    $('.menu-mobile').slideToggle('show')
+})
